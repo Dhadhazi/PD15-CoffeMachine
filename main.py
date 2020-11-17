92ccf316c7814f72ede5bd501f1e1db03a318318
Dhadhazi <dhadhazi@gmail.com>
2020-11-17 10:29:45 +0100

    Resource checker

--- main.py
+++ main.py
@@ -29,3 +29,29 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+def resourceCheck(choice):
+    if (MENU[choice]["ingredients"]["water"] > resources["water"]):
+        return "Sorry there is not enough water."
+    elif (MENU[choice]["ingredients"]["milk"] > resources["milk"]):
+        return "Sorry there is not enough milk."
+    elif (MENU[choice]["ingredients"]["coffee"] > resources["coffee"]):
+        return "Sorry there is not enough coffee."
+    else:
+        return "all cool bro"
+
+choice = ""
+
+while choice != "off":
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+    money = 0
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "report":
+        print(f"Water: {water}\nMilk: {milk}\nCoffee: {coffee}\nMoney: {money}")
+
+    if choice == "espresso" or choice == "latte" or choice == "cappuccino":
+        print(resourceCheck(choice))
+
