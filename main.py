1ad22823626466c51cdbf0f1ba3d03ac37588ad1
Dhadhazi <dhadhazi@gmail.com>
2020-11-17 14:08:57 +0100

    Project Done

--- main.py
+++ main.py
@@ -28,18 +28,42 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0
 }
 
 
-def resourceCheck(choice):
-    if (MENU[choice]["ingredients"]["water"] > resources["water"]):
-        return "Sorry there is not enough water."
-    elif (MENU[choice]["ingredients"]["milk"] > resources["milk"]):
-        return "Sorry there is not enough milk."
-    elif (MENU[choice]["ingredients"]["coffee"] > resources["coffee"]):
-        return "Sorry there is not enough coffee."
+def drinkMaker(choice):
+    global resources;
+    for item in MENU[choice]["ingredients"]:
+        resources[item] -= MENU[choice]["ingredients"][item]
+    resources["money"] += MENU[choice]["cost"]
+    print(f"Here is your {choice}. Enjoy!")
+
+
+def coinProcessor(choice):
+    total = int(input("how many quarters?") or "0")*0.25
+    total += int(input("how many dimes?") or "0")*0.1
+    total += int(input("how many nickles?") or "0")*0.05
+    total += int(input("how many pennies?") or "0")*0.01
+    change = total - MENU[choice]["cost"]
+    if change < 0:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
     else:
-        return "all cool bro"
+        if change > 0:
+            change = round(change, 2)
+            print(f"Here is the ${change} back")
+        return True
+
+
+
+def resourceCheck(choice):
+    for item in MENU[choice]["ingredients"]:
+        if MENU[choice]["ingredients"][item] > resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
+
 
 choice = ""
 
@@ -47,11 +71,13 @@ while choice != "off":
     water = resources["water"]
     milk = resources["milk"]
     coffee = resources["coffee"]
-    money = 0
+    money = resources["money"]
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == "report":
         print(f"Water: {water}\nMilk: {milk}\nCoffee: {coffee}\nMoney: {money}")
 
     if choice == "espresso" or choice == "latte" or choice == "cappuccino":
-        print(resourceCheck(choice))
+        if resourceCheck(choice):
+            if coinProcessor(choice):
+                drinkMaker(choice)
 
